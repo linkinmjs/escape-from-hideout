0b52935d511d40742d6c262063474049338a8b44
unknown <linkinmjs@gmail.com>
2025-07-09 09:48:18 -0300

    Making a better jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,49 +1,52 @@
 extends CharacterBody2D
 
+@export var move_speed = 200
+
+@export var jump_height: float
+@export var jump_time_to_peak: float
+@export var jump_time_to_descent: float
+
+@onready var jump_velocity: float = ((2.0 * jump_height) / jump_time_to_peak) * -1
+@onready var jump_gravity: float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1
+@onready var fall_gravity: float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1
 @onready var player = $PlayerSprite
-
-const SPEED = 80
-const GRAVITY = 5
-const JUMP_POWER = -1800
-const FLOOR = Vector2(0,-1)
-
-var acceleration = 0.1
 
 func _ready() -> void:
 	add_to_group("player")
 
-func _physics_process(delta: float) -> void:
+func _physics_process(delta):
 	if GameManager.is_dead == false:
+		velocity.y += get_player_gravity() * delta
+		velocity.x = get_input_velocity() * move_speed
 		
-		if Input.is_action_pressed("ui_right"):
-			velocity.x = SPEED
-			player.scale.x = 1
-			player.play("walk")
-		elif Input.is_action_pressed("ui_left"):
-			velocity.x = -SPEED
-			player.scale.x = -1
-			player.play("walk")
-		else:
-			velocity.x = 0
-			player.play("idle")
-			
-		if Input.is_action_just_pressed("ui_jump"):
-			velocity.y = JUMP_POWER * acceleration
-			acceleration = acceleration + 0.1
-			player.play("jump")
-			#$sounds/burnSound.play()
-		else:
-			acceleration = 0.1
+		if Input.is_action_just_pressed("ui_jump") and is_on_floor():
+			jump()
 		
-		velocity.y += GRAVITY
-		set_velocity(velocity)
-		set_up_direction(FLOOR)
+		update_animation()
 		move_and_slide()
-		checkcollision()
-		
-func checkcollision()-> void:
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		if collision.get_collider() is TileMapLayer:
-			if collision.get_collider().get("name")=="deadzone":
-				GameManager.is_dead == true
+
+func get_player_gravity() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
+
+func jump():
+	velocity.y = jump_velocity
+
+func get_input_velocity() -> float:
+	var horizontal := 0.0
+	
+	if Input.is_action_pressed("ui_left"):
+		horizontal -= 1.0
+		player.scale.x = -1
+	elif Input.is_action_pressed("ui_right"):
+		horizontal += 1.0
+		player.scale.x = 1
+	
+	return horizontal
+
+func update_animation():
+	if not is_on_floor():
+		player.play("jump")
+	elif velocity.x != 0:
+		player.play("walk")
+	else:
+		player.play("idle")
