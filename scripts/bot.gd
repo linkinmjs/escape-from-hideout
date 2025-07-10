ba8c067b06d62d815172fc50d22493fbc097ac38
unknown <linkinmjs@gmail.com>
2025-07-09 23:10:41 -0300

    Add level theme

--- scripts/bot.gd
+++ scripts/bot.gd
@@ -23,7 +23,7 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if not active_player: return
 	
-	if GameManager.is_dead == false:
+	if GameManager.is_dead == false and GameManager.is_mounted:
 		velocity.y += get_bot_gravity() * delta
 		velocity.x = get_input_velocity() * move_speed
 			
