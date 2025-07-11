8fda69ffbe8ccfe3d07f4c182cd8fb629a362487
unknown <linkinmjs@gmail.com>
2025-07-11 20:41:36 -0300

    Add respawn logic for doors

--- scripts/level_manager.gd
+++ scripts/level_manager.gd
@@ -5,10 +5,11 @@ var door_spawn = 0
 var doors_blocked = [2, 4, 6, 8]
 var doors_opened = [2, 4, 6, 8]
 
-func change_level(next_level: int):
-	actual_level = next_level
-	get_tree().change_scene_to_file("res://scenes/levels/level0%d.tscn" % next_level)
+var player_spawn_position = Vector2()
 
+func change_level(new_level: int):
+	actual_level = new_level
+	get_tree().change_scene_to_file("res://scenes/levels/levelTest0%d.tscn" % new_level)
 
 func check_doors_blocked():
 	var doors_node = get_node("Doors")
--- scripts/levels/level_test.gd
+++ scripts/levels/level_test.gd
@@ -3,10 +3,17 @@ extends Node2D
 var bot_position = Vector2(106.0, 37.0)
 
 func _ready() -> void:
-	if !GameManager.is_mounted:
-		$Bot01.global_position= bot_position
-	elif GameManager.is_mounted:
-		$Bot01.queue_free()
-	
+	spawn_player()
+	spawn_bot()
+
+func spawn_player():
+	var doors_node = get_node("Doors")
+	for door in doors_node.get_children():
+		if door.has_method("get_number"):
+			var number = door.get_number()
+			print("Door number: %d" % number) 
+			if number == LevelManager.door_spawn:
+				if door.has_method("get_spawn_position"): $Player.global_position = door.get_spawn_position()
+
 func spawn_bot():
 	pass
