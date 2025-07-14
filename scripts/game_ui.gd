e7a80e315fcbb04d09c3364b885047bcae02e51a
unknown <linkinmjs@gmail.com>
2025-07-13 23:19:38 -0300

    Add some modifications for UI

--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -1,10 +1,22 @@
 extends CanvasLayer
-
-var life_label:String = "Vidas: " + str(GameManager.lifes)
-var level_label:String = "Nivel: " + str(LevelManager.actual_level)
-var objective_label:String = "Objetivos: " + str(10) + "/" + str(10)
-
+	
 func _physics_process(delta: float) -> void:
+	var life_label:String = "Vidas: " + str(GameManager.lifes)
+	var level_label:String = "Nivel: " + str(LevelManager.actual_level)
+	var objective_label:String = "Objetivos: " + str(10) + "/" + str(10)
+	
 	$ColorRect/HBoxContainer/lifes.text = life_label
 	$ColorRect/HBoxContainer/level.text = level_label
 	$ColorRect/HBoxContainer/objective.text = objective_label
+	if !GameManager.is_mounted:
+		$HBoxContainer/InactiveWeapons.visible = false
+
+func _on_label_timer_timeout() -> void:
+	var inactive_weapons_label = $HBoxContainer/InactiveWeapons
+	var inactive_weapons_timer = $HBoxContainer/Label/LabelTimer
+	
+	if GameManager.is_mounted:
+		if inactive_weapons_label.visible:
+			inactive_weapons_label.visible = false
+		elif !inactive_weapons_label.visible:
+			inactive_weapons_label.visible = true
